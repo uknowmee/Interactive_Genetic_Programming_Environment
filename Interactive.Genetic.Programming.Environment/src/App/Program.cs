8ac7fb896d8d6325ce414a9dd86eef24e61c5b6f
Uknowme <michalpalucki@icloud.com>
2023-12-11 18:02:15 +0100

    Add .net 8 blueprint for WinForms app

---
+++ Interactive.Genetic.Programming.Environment/src/App/Program.cs
@@ -0,0 +1,11 @@
+namespace App;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}
