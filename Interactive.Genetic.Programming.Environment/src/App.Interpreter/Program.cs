98feb05505d413883528cf333d48573cc4c2b888
Uknowme <michalpalucki@icloud.com>
2023-12-12 03:17:14 +0100

    Create Interpreter

--- Interactive.Genetic.Programming.Environment/src/App.Interpreter/Program.cs
+++ Interactive.Genetic.Programming.Environment/src/App.Interpreter/Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Interpreter;
 
-Console.WriteLine("Hello, World!");
+const string program = "x_0 = read();\n" +
+                       "if ((42 > (9 * (56 * 78)))) {\n" +
+                       "\tx_1 = x_0;\n" +
+                       "\twrite(x_0);\n" +
+                       "}\n" +
+                       "x_0 = (((9 * (2 * 45)) - 12) - 9);\n" +
+                       "write(x_0);";
+
+const string program2 = "x_0 = read();\n" +
+                        "if ((42 > (9 * (56 * 78)))) {\n" +
+                        "\tx_1 = x_0;\n" +
+                        "\twrite(x_0);\n" +
+                        "}\n" +
+                        "x_0 = (((9 * (2 * 45)) - 12) - 9);\n" +
+                        "for (x_3 = 92, (10 < 20), -x_0) {\n\twrite(x_3);\n}" +
+                        "write(x_0);";
+
+var inputs = new List<double> { 0.0, 2.0 };
+
+for (var i = 0; i < 1000; i++)
+{
+    try
+    {
+        IInterpreterService interpreter = new InterpreterService(i % 50 == 0
+            ? program2
+            : program
+        );
+        interpreter.Run(inputs);
+
+        Console.Out.WriteLine(
+            $"{i} Program finished running. " +
+            $"{nameof(interpreter.IsFinished)}: {interpreter.IsFinished}. " +
+            $"{nameof(interpreter.Outputs)}: {string.Join(", ", interpreter.Outputs)}"
+        );
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+return;
---
+++ Interactive.Genetic.Programming.Environment/src/Services/Interpreter/InterpreterService.cs
@@ -0,0 +1,58 @@
+﻿using System.Data;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using Interpreter.Exceptions;
+
+namespace Interpreter;
+
+public class InterpreterService : IInterpreterService
+{
+    public bool IsFinished { get; private set; } = false;
+    public IEnumerable<double> Outputs => _visitor?.Outputs ?? [];
+
+    private Visitor? _visitor = null;
+    private readonly IParseTree _parseTree;
+
+    private const int DueTime = 10;
+    private CancellationTokenSource? _cancellationTokenSource;
+    private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+
+    public void Run(List<double> inputs)
+    {
+        IsFinished = false;
+        _visitor = new Visitor(inputs);
+
+        try
+        {
+            _cancellationTokenSource = new CancellationTokenSource(DueTime);
+            _semaphore.Wait(_cancellationTokenSource.Token);
+
+            Task.Run(() =>
+                {
+                    _visitor?.Visit(_parseTree);
+                    IsFinished = true;
+                }, _cancellationTokenSource.Token
+            ).Wait(_cancellationTokenSource.Token);
+        }
+        catch (OperationCanceledException exception)
+        {
+            throw new ExecutionTimeExceededException("Time limit for interpreting the program has exceeded.", exception);
+        }
+        catch (Exception exception)
+        {
+            throw new InterpreterServiceException("An error occurred while interpreting the program.", exception);
+        }
+    }
+
+    public InterpreterService(string program)
+    {
+        var lexer = new IntGrammarLexer(new AntlrInputStream(program));
+        var parser = new IntGrammarParser(new CommonTokenStream(lexer));
+        _parseTree = parser.program();
+
+        if (parser.NumberOfSyntaxErrors > 0)
+        {
+            throw new SyntaxErrorException("Syntax error in program.");
+        }
+    }
+}
