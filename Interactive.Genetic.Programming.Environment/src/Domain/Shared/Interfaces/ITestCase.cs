10a18e81459cc2c548726ffbbbe11ee71b9891a6
Uknowme <michalpalucki@icloud.com>
2024-01-14 12:40:05 +0100

    Set release debug symbols to none

--- Interactive.Genetic.Programming.Environment/src/Domain/Shared/Interfaces/ITestCase.cs
+++
@@ -1,8 +0,0 @@
-namespace Shared.Interfaces;
-
-public interface ITestCase
-{
-    public List<double> Input { get; }
-    public List<double> Output { get; }
-    public List<double> Predicted { get; }
-}
