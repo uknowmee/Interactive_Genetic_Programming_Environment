4acb4831c78d3655fb6a5d9325c1730bbea1b958
Uknowme <michalpalucki@icloud.com>
2024-01-15 22:30:30 +0100

    Clear up demo

--- Interactive.Genetic.Programming.Environment/demo/LangTests/Program.cs
+++ Interactive.Genetic.Programming.Environment/demo/LangTests/Program.cs
@@ -1,15 +1,3 @@
-﻿var intList = new List<int> { 1, 2, 3, 4, 5 };
+﻿// See https://aka.ms/new-console-template for more information
 
-intList.Set(3, 666);
-
-Console.Out.WriteLine("intList: " + string.Join(", ", intList));
-
-public static class ListExtensions
-{
-    public static void Set<T>(this List<T> list, T toRemove, T item)
-    {
-        var idx = list.IndexOf(toRemove);
-        list.RemoveAt(idx);
-        list.Insert(idx, item);
-    }
-}
+Console.WriteLine("Hello, World!");
